aada55a97b063ee99f24291d133b0a3ef8f967f0
belvis <elvis@edi.gmbh>
2023-04-25 17:41:51 +0200

    Completed first test run successful

--- tests/saucedemo/saucedemo_lib.py
+++ tests/saucedemo/saucedemo_lib.py
@@ -1,3 +1,36 @@
 """
-For writting lib for test
+Library classes of methods used for sauce demo tests.
 """
+
+from saucedemo_selenium_lib.saucedemo_core.saucedemo_page import BaseSaucedemoPage
+from src.locators import SauceDemoProductsPageLocators
+
+
+class SaucedemoProductsLib(BaseSaucedemoPage):
+    """Class for lirbary of methods used in the Products page of Saucedemo webapp"""
+    def view_products_catalogue(self, product_names: list):
+        """
+        Checks the presence of the products title element
+        and check products in the product list.
+
+        Args:
+            product_names: list
+
+        Return:
+            None
+
+        """
+        self.__check_presence_of_product_title()
+        for product_name in product_names:
+            self.verify_if_product_is_present(
+                product_name=product_name,
+            )
+
+    def __check_presence_of_product_title(self):
+        """
+        Assert the products title element on the page.
+
+        """
+        assert self.saucedemo_utils.is_element_available(
+            locator=SauceDemoProductsPageLocators.PRODUCTS_TITLE,
+        )
--- tests/saucedemo/test_saucedemo.py
+++ tests/saucedemo/test_saucedemo.py
@@ -1,3 +1,42 @@
-"""
-For writting tests
-"""
+import unittest
+
+from saucedemo_selenium_lib.config import TestConfig
+from saucedemo_selenium_lib.saucedemo_utils import saucedemo_utils as sl
+from tests.saucedemo.saucedemo_lib import (
+    SaucedemoProductsLib,
+)
+
+
+class BaseTestClass(unittest.TestCase):
+    config = TestConfig(host_index=0)
+    headless = True
+    product_names = [
+        "Sauce Labs Backpack",
+        "Sauce Labs Bike Light",
+        "Sauce Labs Bolt T-Shirt",
+        "Sauce Labs Fleece Jacket",
+        "Sauce Labs Onesie",
+        "Test.allTheThings() Tshirt",
+    ]
+
+class BrowsingAndFilteringCatalogue(BaseTestClass):
+    def setUp(self):
+        saucedemo_utils = sl.SaucedemoUtils(
+            host_url=self.config.host_url,
+            username=self.config.username,
+            password=self.config.password,
+            headless=self.headless,
+            browser=self.config.browser,
+            grid=self.config.grid,
+        )
+        self.saucedemo_product_lib = SaucedemoProductsLib(saucedemo_utils)
+        self.saucedemo_product_lib.log_in_saucedemo()
+
+    def tearDown(self):
+        self.saucedemo_product_lib.reset_app_state()
+        self.saucedemo_product_lib.close_browser()
+
+    def test_sd_uc01_001_view_the_products_catalogue(self):
+        self.saucedemo_product_lib.view_products_catalogue(
+            product_names=self.product_names,
+        )
