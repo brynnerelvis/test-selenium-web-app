7e7874603e18622b2d98b68bd92c5e4fa7a052b8
belvis <elvis@edi.gmbh>
2023-04-27 10:44:31 +0200

    Updates for more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,102 @@
 """"
 For adding tests fixtures
 """
+
+import os
+
+import pytest
+
+from saucedemo_selenium_lib.config import TestConfig
+from saucedemo_selenium_lib.data_models import WebBrowsers
+
+
+def pytest_addoption(parser):
+    parser.addoption("--host_index", action="store", default="0")
+    parser.addoption("--headless", action="store", default="1")
+    parser.addoption("--proxy", action="store", default=None)
+    parser.addoption("--runner", action="store", default="app")
+    parser.addoption("--url", action="store", default="https://www.saucedemo.com/")
+    parser.addoption("--username", action="store", default="standard_user")
+    parser.addoption("--password", action="store", default="secret_sauce")
+    parser.addoption(
+        "--browser",
+        action="store",
+        default="chrome",
+        help="Web browser that selenium tests will be run on. Available options include 'chrome' and 'firefox'. "
+        "Default is chrome",
+    )
+
+
+def pytest_sessionstart(session):
+    """
+    Called after the Session object has been created and
+    before performing collection and entering the run test loop.
+    """
+
+
+def pytest_sessionfinish(session, exitstatus):
+    """
+    Called after whole test run finished, right before
+    returning the exit status to the system.
+    """
+
+
+@pytest.fixture(scope="session")
+def host_index(request):
+    index_value = request.config.option.host_index
+    return index_value
+
+
+@pytest.fixture(scope="session", autouse=True)
+def create_test_config(request):
+    runner = request.config.option.runner
+    browser_arg = request.config.option.browser
+    if browser_arg == "firefox":
+        browser = WebBrowsers.FIREFOX
+    elif browser_arg == "chrome":
+        browser = WebBrowsers.CHROME
+    else:
+        raise AssertionError(f"--browser argument: {browser_arg}")
+    if runner == "app":
+        url = request.config.option.url
+        username = request.config.option.username
+        password = request.config.option.password
+        config_test = TestConfig(
+            host_url=url, username=username, password=password, browser=browser
+        )
+    else:
+        index_value = request.config.option.host_index
+
+        if index_value is not None:
+
+            index_value = int(index_value)
+            config_test = TestConfig(host_index=index_value, browser=browser)
+            if not os.path.exists(config_test.config_file_path):
+                # check that the config path exists
+                raise AssertionError(
+                    f"config.yml file containing Saucedemo Instance configs not found. Please add it in the given path: {config_test.config_file_path}.",
+                )
+        else:
+            raise AssertionError("the host index Value not defined")
+    return config_test
+
+
+@pytest.fixture(scope="class", autouse=True)
+def config_class(request, create_test_config):
+    """Get commandline options and Change test classes attributes during runtime.
+    Set autouse to True so that pytest automically decorate the Test case classes"""
+    headless = request.config.option.headless
+    if headless is not None:
+        if int(headless) == 1:
+            request.cls.headless = True
+        else:
+            request.cls.headless = False
+    proxy = request.config.option.proxy
+    request.cls.proxy = proxy
+    request.cls.config = create_test_config
+
+
+def pytest_html_report_title(report):
+    """Change HTML Report title"""
+
+    report.title = f"Selenium Tests for Report"
--- tests/saucedemo/saucedemo_lib.py
+++ tests/saucedemo/saucedemo_lib.py
@@ -1,9 +1,10 @@
 """
 Library classes of methods used for sauce demo tests.
 """
+from selenium.webdriver.common.by import By
 
 from saucedemo_selenium_lib.saucedemo_core.saucedemo_page import BaseSaucedemoPage
-from src.locators import SauceDemoProductsPageLocators
+from src.locators import SauceDemoProductsPageLocators, SauceDemoProductsDetailLocators
 
 
 class SaucedemoProductsLib(BaseSaucedemoPage):
@@ -20,7 +21,7 @@ class SaucedemoProductsLib(BaseSaucedemoPage):
             None
 
         """
-        self.__check_presence_of_product_title()
+        assert self.__check_presence_of_product_title()
         for product_name in product_names:
             self.verify_if_product_is_present(
                 product_name=product_name,
@@ -28,9 +29,179 @@ class SaucedemoProductsLib(BaseSaucedemoPage):
 
     def __check_presence_of_product_title(self):
         """
-        Assert the products title element on the page.
+        Checks the products title element on the page
+        and returns True if found, else returns False.
+
+        Return:
+            Boolean
 
         """
-        assert self.saucedemo_utils.is_element_available(
+        return self.saucedemo_utils.is_element_available(
             locator=SauceDemoProductsPageLocators.PRODUCTS_TITLE,
+        )
+
+    def __is_price_list_sorted(self, price_list, sort_type):
+        """
+        Checks the product price list sorted according to
+        the sort type, and returns a boolean value:
+            True if sorted
+            False if not sorted
+
+        Args:
+            price_list: list
+            sort_type: str
+
+        Return:
+            boolean
+
+        """
+        # Convert price strings to float values
+        float_prices = [float(price[1:]) for price in price_list]
+
+        if sort_type == "Price (low to high)":
+            return all(price1 <= price2 for price1, price2
+                       in zip(float_prices, float_prices[1:]))
+        elif sort_type == "Price (high to low)":
+            return all(price1 >= price2 for price1, price2
+                       in zip(float_prices, float_prices[1:]))
+        else:
+            raise ValueError("Invalid sort type")
+
+    def __is_product_names_list_sorted(self, product_names_list: list, sort_type: str):
+        """
+        Checks the product names list sorted according to
+        the sort type, and returns a boolean value:
+            True if sorted
+            False if not sorted
+
+        Args:
+            product_names_list: list
+            sort_type: str
+
+        Return:
+            boolean
+
+        """
+        if sort_type == "Name (A to Z)":
+            return all(product1 <= product2 for product1, product2
+                       in zip(product_names_list, product_names_list[1:]))
+        elif sort_type == "Name (Z to A)":
+            return all(product1 >= product2 for product1, product2
+                       in zip(product_names_list, product_names_list[1:]))
+        else:
+            raise ValueError("Invalid sort order")
+
+    def sort_products(self, sort_type: str):
+        """
+        Sorts the products by selecting a sort type
+        from the sort drop down list and assert if the
+        products are sorted.
+
+        Args:
+            sort_type: str
+
+        Return:
+            None
+
+        """
+        # sort the product list based on the sort type
+        self.sort_product_list(sort_type=sort_type)
+
+        # assert if the product list was sorted
+        if sort_type == "Name (A to Z)" or sort_type == "Name (Z to A)":
+            product_names_list = self.saucedemo_utils.get_product_names()
+            assert self.__is_product_names_list_sorted(
+                sort_type=sort_type,
+                product_names_list=product_names_list,
+            )
+        elif sort_type == "Price (low to high)" or sort_type == "Price (high to low)":
+            price_list = self.saucedemo_utils.get_product_prices()
+            assert self.__is_price_list_sorted(
+                sort_type=sort_type,
+                price_list=price_list
+            )
+        else:
+            raise ValueError("Invalid sort order")
+
+    def click_product_image(self, product_name):
+        """
+        Clicks on the product image based on the product
+        name.
+
+        Args:
+            product_name: str
+
+        Return:
+            None
+
+        """
+        self.select_product_by_image(product_name)
+
+    def click_product_name(self, product_name):
+        """
+        Clicks on the product name.
+
+        Args:
+            product_name: str
+
+        Return:
+            None
+
+        """
+        self.select_product_by_name(product_name)
+
+
+class SaucedemoProductDetailLib(BaseSaucedemoPage):
+    """Class for the product detail page of Saucedemo webapp"""
+
+    def verify_product_detail_is_open(self, product_name: str):
+        """
+        Assert the name of the product and the back to products
+        button in the product detail page.
+
+        Args:
+             product_name: str
+
+        Return:
+            None
+
+        """
+        assert self.__check_presence_of_back_to_products_button()
+        assert self.__check_presence_of_detail_product_name(
+            product_name=product_name,
+        )
+
+
+    def __check_presence_of_detail_product_name(self, product_name: str):
+        """
+        Checks the product name on the detail page and returns
+        True if found, else returns False.
+
+        Args:
+            product_name: str
+
+        Return:
+            Boolean
+
+        """
+        detail_product_name_locator = (
+            By.XPATH,
+            f"//div[contains(@class, 'inventory_details_name') "
+            f"and contains(., '{product_name}')]"
+        )
+        return self.saucedemo_utils.is_element_available(
+            locator=detail_product_name_locator,
+        )
+
+    def __check_presence_of_back_to_products_button(self):
+        """
+        Checks the back to products button element on the
+        page and returns True if found, else returns False.
+
+        Return:
+            Boolean
+
+        """
+        return self.saucedemo_utils.is_element_available(
+            locator=SauceDemoProductsDetailLocators.BACK_TO_PRODUCTS_BUTTON,
         )
